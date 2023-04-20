3537d775f3d75eacb0f8eb4c84ec43782aabc0b1
Vaishak Nair <nairvaishak101@gmail.com>
2023-04-20 23:57:23 +0530

    Menu Model Completed

--- server/controller/menuController.js
+++ server/controller/menuController.js
@@ -19,12 +19,11 @@ exports.getMenu = async (req, res) => {
 
 exports.addMenuItem = async (req, res) => {
   try {
-    const newItem = await Menu.create(req.body);
-
+    const menuItem = await Menu.create(req.body);
     res.status(201).json({
       status: "success",
       data: {
-        menuItem: newItem
+        menuItem: menuItem
       }
     })
   }
@@ -36,9 +35,38 @@ exports.addMenuItem = async (req, res) => {
   }
 }
 
-exports.updateMenuItem = (req, res) => {
-  res.status(201).json({ status: 201, message: "Update Item in Menu" })
+exports.updateMenuItem = async (req, res) => {
+  try {
+    const menuItem = await Menu.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).json({
+      status: 'success',
+      data: {
+        menuItem
+      }
+    })
+  }
+  catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      error: err
+    })
+  }
 }
-exports.deleteMenuItem = (req, res) => {
-  res.status(201).json({ status: 201, message: "Delete Item from Menu" })
+
+exports.deleteMenuItem = async (req, res) => {
+  try {
+    const menuItem = await Menu.findByIdAndDelete(req.params.id, req.body)
+    res.status(200).json({
+      status: 'success',
+      data: {
+        menuItem
+      }
+    })
+  }
+  catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      error: err
+    })
+  }
 }
