be73943911e9e08dd60c58046c81bb34c27c5972
Vaishak Nair <nairvaishak101@gmail.com>
2023-04-22 03:03:29 +0530

    Order Sort and filter fetch added

--- server/controller/orderController.js
+++ server/controller/orderController.js
@@ -3,13 +3,13 @@ const Order = require('./../models/orderModel');
 exports.getAllOrder = async (req, res) => {
   try {
     let query = Order.find();
-    if (req.query.month) {
-      let [idObj] = await Order.aggregate([
-        { $project: { month: { $month: '$orderDate' } } },
-        { $match: { month: Number(req.query.month) } }
-      ])
-      query = Order.find(idObj._id)
-    }
+    // if (req.query.month) {
+    //   let [idObj] = await Order.aggregate([
+    //     { $project: { month: { $month: '$orderDate' } } },
+    //     { $match: { month: Number(req.query.month) } }
+    //   ])
+    //   query = Order.find(idObj._id)
+    // }
     const order = await query;
     res.status(200).json({
       status: "success",
@@ -24,6 +24,53 @@ exports.getAllOrder = async (req, res) => {
     })
   }
 }
+exports.getTrending = async (req, res) => {
+  try {
+    const start = new Date(req.query.year, req.query.month - 1, 1);
+    const end = new Date(req.query.year, req.query.month, 1);
+
+    let result = await Order.find({
+      orderDate: { $gte: start, $lt: end }
+    });
+
+    const titleArray = result.map(el => el.orderTitle)
+
+    function count(arr) {
+      let o = {}, i;
+      for (i = 0; i < arr.length; ++i) {
+        if (o[arr[i]]) ++o[arr[i]];
+        else o[arr[i]] = 1;
+      }
+      return o;
+    }
+
+    function weight(arr_in) {
+      let o = count(arr_in),
+        arr = [], i;
+      for (i in o) arr.push({ value: i, weight: o[i] });
+      arr.sort(function (a, b) {
+        return a.weight < b.weight;
+      });
+      return arr;
+    }
+
+    let filteredResult = weight(titleArray);
+
+    res.status(200).json({
+      status: "success",
+      result: filteredResult.length,
+      trendingItems: filteredResult
+    })
+  }
+  catch (err) {
+    console.log(err)
+    res.status(400).json({
+      status: "Failed",
+      error: err
+    })
+  }
+}
+
 exports.addOrder = async (req, res) => {
   try {
     const order = await Order.create(req.body);
--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controller/orderController')
 
+
+router.route('/trending').get(orderController.getTrending)
 router
   .route('/')
   .get(orderController.getAllOrder)
