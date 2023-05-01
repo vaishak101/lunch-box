4da1ade6e4f54baa4927967b4e1d4e5554a7e8ab
Vaishak Nair <nairvaishak101@gmail.com>
2023-05-02 02:16:31 +0530

    Live DB added

--- server/server.js
+++ server/server.js
@@ -4,7 +4,10 @@ const app = require("./app")
 
 dotenv.config({ path: './config.env' });
 
-const db = process.env.DATABASE_LOCAL
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+)
 mongoose.connect(db).then(con => {
   console.log("Connection Successfull");
 })
