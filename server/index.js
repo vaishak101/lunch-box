28777a65ec5def1f392599f68f518f6f36401605
Vaishak Nair <nairvaishak101@gmail.com>
2023-05-02 14:58:11 +0530

    Small fixes

--- server/index.js
+++ server/index.js
@@ -3,24 +3,34 @@ const dotenv = require('dotenv');
 const app = require("./app")
 let server;
 dotenv.config({ path: './config.env' });
-
 const db = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 )
-mongoose.connect(db).then(con => {
-  console.log("Connection Successfull");
-  server = app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(db);
+    console.log(`MongoDB Connected`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+//Connect to the database before listening
+connectDB().then(() => {
+  server = app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
   })
 })
-
-const port = process.env.PORT || 3000;
 
 process.on('unhandledRejection', err => {
   console.log('Unhandled Rejection');
   console.log(err.name, err.message);
-  server.close(() => {
+  app.close(() => {
     process.exit(1);
   });
 });
